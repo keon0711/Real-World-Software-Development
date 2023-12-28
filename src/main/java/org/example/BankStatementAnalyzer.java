6e5425b94c30fe1c83d20a385717913930f314ce
Keon Kim <fnc6488@gmail.com>
2023-12-28 12:23:43 +0900

    BankStatementCSVParser를 인터페이스와 구현체로 분리

--- src/main/java/org/example/BankStatementAnalyzer.java
+++ src/main/java/org/example/BankStatementAnalyzer.java
@@ -8,27 +8,22 @@ import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BankTransactionAnalyzer {
+public class BankStatementAnalyzer {
     private static final String RESOURCE = "src/main/resources/";
-    private static final BankStatementCSVParser bankStatementCSVParser = new BankStatementCSVParser();
 
-    public static void main(String[] args) throws IOException {
-
-
-        final String fileName = args[0];
+    public void analyze(final String fileName, final BankStatementParser bankStatementParser) throws IOException {
         final Path path = Paths.get(RESOURCE + fileName);
         final List<String> lines = Files.readAllLines(path);
 
-        final List<BankTransaction> bankTransactions = bankStatementCSVParser.parseLinesFromCSV(lines);
+        final List<BankTransaction> bankTransactions = bankStatementParser.parseLinesFrom(lines);
         final BankStatementProcessor bankStatementProcessor = new BankStatementProcessor(bankTransactions);
 
         collectSummary(bankStatementProcessor);
     }
 
-    private static void collectSummary(BankStatementProcessor bankStatementProcessor) {
+    private void collectSummary(BankStatementProcessor bankStatementProcessor) {
         System.out.println("The total for transactions is " + bankStatementProcessor.calculateTotalAmount());
         System.out.println("The total for transactions in January is " + bankStatementProcessor.calculateTotalInMonth(Month.JANUARY));
         System.out.println("The total salary received is " + bankStatementProcessor.calculateTotalForCategory("Salary"));
-
     }
 }
